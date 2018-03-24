8c1520ca8c3ee728ad910626d7070a292c281235
ESchae <ellifant_smile@web.de>
2018-03-24 22:23:24 +0100

    added id_by_color

--- src/python/config.py
+++ src/python/config.py
@@ -1,9 +1,16 @@
 ids = {
-    0: {'color': 'white', 'type': 'octagon'},
-    1: {'color': 'white', 'type': 'octagon'},
-    2: {'color': 'white', 'type': 'octagon'},
-    3: {'color': 'white', 'type': 'octagon'},
+    0: {'color': 'red', 'type': 'octagon'},
+    1: {'color': 'blue', 'type': 'octagon'},
+    2: {'color': 'yellow', 'type': 'octagon'},
+    3: {'color': 'green', 'type': 'octagon'},
     4: {'color': 'white', 'type': 'octagon'},
     5: {'color': 'white', 'type': 'octagon'},
     6: {'color': 'white', 'type': 'octagon'}
+}
+
+id_by_color = {
+    'red': 0,
+    'blue': 1,
+    'yellow': 2,
+    'green': 3
 }
