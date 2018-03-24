864c63f1a059f058824cff2d7d53bed92a6bebaa
ESchae <elke.schaechtele@web.de>
2018-03-24 19:31:49 +0100

    added modules for handling changes on whiteboard

---
+++ element.py
@@ -0,0 +1,52 @@
+from osc_handler import OSCHandler
+from config import ids
+import logging
+
+
+class Element(object):
+    """ Abstract class representing an element on the Whiteboard. """
+
+    def __init__(self, id, rotation, x, y):
+        self.id = id
+        self.rotation = rotation
+        self.x = x
+        self.y = y
+        self.color = ids[self.id]['color']
+        self.type = ids[self.id]['type']
+        self.osc = OSCHandler()
+
+    def __str__(self):
+        return 'Element %s' % self.id
+
+    def update_parameters(self, threshold=0.3, **params):
+        for parameter, value in params.items():
+            if abs(self.__dict__[parameter] - value) >= threshold:
+                self.__dict__[parameter] = value
+
+    @staticmethod
+    def factory(**kwargs):
+        element_id = kwargs['id']
+        element_type = ids[element_id]['type']
+        if element_type == 'octagon':
+            return Octagon(**kwargs)
+
+
+class Octagon(Element):
+
+    def __init__(self, id, rotation, x, y):
+        super().__init__(id, rotation, x, y)
+        self.logger = logging.getLogger(__name__)
+
+    def __str__(self):
+        return 'Octagon %s (%s): %.2f rotation  %.2f x  %.2f y' \
+               % (self.id, self.color, self.rotation, self.x, self.y)
+
+    def set_parameters(self, rotation, x, y):
+        self.x = x
+        self.y = y
+        self.rotation = rotation
+
+    def send_audio_parameters(self):
+        args = [self.id, self.rotation, self.x, self.y]
+        self.logger.info('Setting parameters: %s' % str(self))
+        self.osc.send_msg(args)
---
+++ whiteboard.py
@@ -0,0 +1,50 @@
+import logging
+from element import Element
+
+
+class Whiteboard(object):
+
+    def __init__(self):
+        self.elements = []
+        self.logger = logging.getLogger(__name__)
+
+    def add_element(self, element):
+        self.elements.append(element)
+        self.logger.info('Added %s' % str(element))
+
+    def delete_element(self, element):
+        self.elements.remove(element)
+        self.logger.info('Deleted %s' % str(element))
+
+    def update_element(self, element, threshold=0.3, **params):
+        element.update_parameters(threshold, **params)
+        self.logger.info('Updated %s' % str(element))
+
+    def get_element(self, element_id):
+        for element in self.elements:
+            if element.id == element_id:
+                return element
+        return None
+
+    def get_element_ids(self):
+        return [element.id for element in self.elements]
+
+    def update(self, new_params):
+        self.delete_removed_elements(new_params)
+        for params in new_params:
+            element = self.get_element(params['id'])
+            if element:
+                self.update_element(element, **params)
+            else:
+                element = Element.factory(**params)
+                self.add_element(element)
+
+    def get_removed_elements(self, new_params):
+        new_element_ids = [params['id'] for params in new_params]
+        removed_elements = [element for element in self.elements
+                            if element.id not in new_element_ids]
+        return removed_elements
+
+    def delete_removed_elements(self, new_params):
+        for element in self.get_removed_elements(new_params):
+            self.delete_element(element)
