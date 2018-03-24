b791f0fe6823bb46ecb9a3f42b5b69931a8cacc2
Florian Becker <beckerfloh@gmail.com>
2018-03-24 23:39:00 +0100

    put circles into dictionary data structure

--- src/python/util.py
+++ src/python/util.py
@@ -8,6 +8,8 @@ def draw_circles(frame, window_name, circles):
         # draw the center of the circle
         cv2.circle(frame,(i[0],i[1]),2,(0,0,255),3)
 
+
+    #cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
     cv2.imshow(window_name, frame)
  
 
