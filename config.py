7fd6c65e3b2f38fa8278670a01aab29655744e75
ESchae <elke.schaechtele@web.de>
2018-03-24 19:31:08 +0100

    added config file for elements

---
+++ config.py
@@ -0,0 +1,9 @@
+ids = {
+    0: {'color': 'white', 'type': 'octagon'},
+    1: {'color': 'white', 'type': 'octagon'},
+    2: {'color': 'white', 'type': 'octagon'},
+    3: {'color': 'white', 'type': 'octagon'},
+    4: {'color': 'white', 'type': 'octagon'},
+    5: {'color': 'white', 'type': 'octagon'},
+    6: {'color': 'white', 'type': 'octagon'}
+}
